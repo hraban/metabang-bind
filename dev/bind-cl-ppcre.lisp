124d60f314244120ceed0178ebe63a7848e4923b
Gary King <gwking@metabang.com>
2009-03-08 20:50:20 -0400

    just moved stuff around
    darcs-hash:20090309005020-3cc5d-f95cdc9ed7b6b54f6d5537c0caec5956e1948720.gz

--- dev/bind-cl-ppcre.lisp
+++ dev/bind-cl-ppcre.lisp
@@ -1,5 +1,29 @@
 (in-package #:metabang.bind.developer)
 
+(defmethod bind-generate-bindings
+    ((kind (eql :re)) variable-form value-form
+     body declarations remaining-bindings)
+  ;; (:re "re" vars)
+  (bind (((regex &rest vars) variable-form)
+	 (gok (gensym "ok"))
+	 (gblock (gensym "block"))
+	 ((:values vars ignores) (bind-fix-nils vars)))
+    `((let ((,gok nil))
+	(block ,gblock
+	  (flet ((doit (,@vars)
+		   ,@(when ignores `((declare (ignore ,@ignores))))
+		   (return-from ,gblock
+		     (progn ,@(bind-macro-helper
+                       remaining-bindings declarations body)))))
+	    (cl-ppcre:register-groups-bind 
+		,vars (,regex ,value-form :sharedp t)
+	      ,(bind-filter-declarations
+		declarations variable-form)
+	      (setf ,gok t)
+	      (doit ,@vars))
+	    (unless ,gok
+	      (doit ,@(make-list (length vars) :initial-element nil)))))))))
+
 #+(or)
 ;; simple but doesn't execute inner code if no bindings found
 ;; which isn't very bind-like
@@ -29,30 +53,6 @@
 		   (return-from ,gblock
 		     ,@(bind-macro-helper
 			remaining-bindings declarations body))))
-	    (cl-ppcre:register-groups-bind 
-		,vars (,regex ,value-form :sharedp t)
-	      ,(bind-filter-declarations
-		declarations variable-form)
-	      (setf ,gok t)
-	      (doit ,@vars))
-	    (unless ,gok
-	      (doit ,@(make-list (length vars) :initial-element nil)))))))))
-
-(defmethod bind-generate-bindings
-    ((kind (eql :re)) variable-form value-form
-     body declarations remaining-bindings)
-  ;; (:re "re" vars)
-  (bind (((regex &rest vars) variable-form)
-	 (gok (gensym "ok"))
-	 (gblock (gensym "block"))
-	 ((:values vars ignores) (bind-fix-nils vars)))
-    `((let ((,gok nil))
-	(block ,gblock
-	  (flet ((doit (,@vars)
-		   ,@(when ignores `((declare (ignore ,@ignores))))
-		   (return-from ,gblock
-		     (progn ,@(bind-macro-helper
-                       remaining-bindings declarations body)))))
 	    (cl-ppcre:register-groups-bind 
 		,vars (,regex ,value-form :sharedp t)
 	      ,(bind-filter-declarations
